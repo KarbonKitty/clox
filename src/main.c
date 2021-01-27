7e61b9d84560f7a184640a8d4d2f50ddc3fdc9f5
KarbonKitty <308lapua.magnum@gmail.com>
2021-01-27 17:06:18 +0100

    Chapter 14.6 - Line information

--- src/main.c
+++ src/main.c
@@ -7,10 +7,10 @@ int main(int argc, const char* argv[]) {
     initChunk(&chunk);
 
     int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT);
-    writeChunk(&chunk, constant);
+    writeChunk(&chunk, OP_CONSTANT, 42);
+    writeChunk(&chunk, constant, 42);
 
-    writeChunk(&chunk, OP_RETURN);
+    writeChunk(&chunk, OP_RETURN, 42);
 
     disassembleChunk(&chunk, "test chunk");
 
